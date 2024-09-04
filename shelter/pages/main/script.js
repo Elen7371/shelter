3edcd0a7b2f5a3707005827900840112f69f58aa
Elen7371 <elena.and.kam@gmail.com>
2024-09-04 18:45:04 +0300

    feat: add animation and functionality for sidebar on main page

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -29,28 +29,40 @@ CAROUSEL.addEventListener("animationend", () => {
   BTN_RIGHT.addEventListener("click", moveRight);
 });
 
-// burger-menu: animation
+// burger-menu: appearance and animation
 
 const BTN_BURGER = document.querySelector(".burger-menu-btn");
 const SIDEBAR = document.querySelector(".header-nav");
 const BODY = document.querySelector("body")
 
-const pressBurger = () => {
+const burgerToggler = () => {
   BTN_BURGER.classList.toggle("burger-logo-turn");
   SIDEBAR.classList.toggle("burger-open");
   SIDEBAR.classList.add("transition-burger");
   BODY.classList.toggle("body-scroll")
+  document.querySelector(".overlay").classList.toggle("burger-overlay");
 }
 
-BTN_BURGER.onclick = function () {
-  
+// BTN_BURGER.addEventListener("click", pressBurger);
+
+BODY.addEventListener("click", pressSomethere);
+
+
+function pressSomethere(event) {
+let target = event.target;
+
+if (target === BTN_BURGER /* клик на кнопку бургера*/ 
+   || target.classList.contains("header-link") /* клик на ссылку */ 
+   || (SIDEBAR.classList.contains("burger-open") && target !== SIDEBAR) /* клик за пределами sidebar при открытом меню */ ) {
+      burgerToggler(); 
+} 
 }
 
 
-BTN_BURGER.addEventListener("click", pressBurger);
 
+// реализовать по бургеру: 
+// 3. поставить бургер на страницу pets
 
-// document.body.addEventListener("click", closeScroll)
 
 
 // попапы старт
