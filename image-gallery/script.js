d52e8556f2d8a7ca773fa3e970cc23e1b9024f7c
Elen7371 <elena.and.kam@gmail.com>
2024-09-29 01:46:32 +0300

    feat: add interactive for elements

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -2,8 +2,8 @@
 
 let url =
   "https://api.unsplash.com/photos/?query=mountain&per_page=12&orientation=landscape&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw";
-  // "https://api.unsplash.com/photos/random?query=spring&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw";
-  // "https://api.unsplash.com/photos/?query=new&per_page=12&orientation=landscape&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw";
+// "https://api.unsplash.com/photos/random?query=spring&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw";
+// "https://api.unsplash.com/photos/?query=new&per_page=12&orientation=landscape&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw";
 
 // собираем список изобрадений на экране
 
@@ -14,10 +14,13 @@ let photoList = document.querySelectorAll(".net-image");
 async function getData() {
   const res = await fetch(url);
   const data = await res.json();
+  showData(data);
+}
+
+function showData(data) {
   for (let i = 0; i < data.length; i++) {
     photoList[i].src = `${data[i].urls.regular}`;
   }
 }
 
 getData();
-
