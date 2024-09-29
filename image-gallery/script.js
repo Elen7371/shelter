86d2eb11684eac8eb9eab2dcbd694e3e0ccc1f75
Elen7371 <elena.and.kam@gmail.com>
2024-09-30 00:16:42 +0300

    feat: add script.js

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,26 +1,47 @@
 // запрос к сервверу unsplash
 
 let url =
-  "https://api.unsplash.com/photos/?query=mountain&per_page=12&orientation=landscape&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw";
-// "https://api.unsplash.com/photos/random?query=spring&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw";
-// "https://api.unsplash.com/photos/?query=new&per_page=12&orientation=landscape&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw";
-
-// собираем список изобрадений на экране
+  "https://api.unsplash.com/search/photos?query=icecream&per_page=12&orientation=landscape&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw";
 
 let photoList = document.querySelectorAll(".net-image");
-
-// забираем данные и вставляем
 
 async function getData() {
   const res = await fetch(url);
   const data = await res.json();
-  showData(data);
-}
-
-function showData(data) {
-  for (let i = 0; i < data.length; i++) {
-    photoList[i].src = `${data[i].urls.regular}`;
-  }
+  const results = data.results;
+  showData(results);
 }
 
 getData();
+
+function showData(results) {
+  for (let i = 0; i < results.length; i++) {
+    photoList[i].src = `${results[i].urls.regular}`;
+    photoList[i].alt = `${results[i].alt_description}`;
+  }
+}
+
+// грустная история о том, как я пыталась, но не смогла(((
+
+// let userQuery = ''
+
+// let input = document.querySelector("input");
+// console.log(input.value)
+
+//   input.onchange = function () {
+//     url = `https://api.unsplash.com/search/photos?${input.value}&per_page=12&orientation=landscape&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw`;
+// };
+
+// let form = document.querySelector("form");
+
+// form.addEventListener("submit", (event) => {
+//   let submitter = event.submitter;
+//   let handler = submitter.id;
+
+// }
+// )
+
+// const data = await getData()
+// showData(data)
+
+// await getData().then(showData);
