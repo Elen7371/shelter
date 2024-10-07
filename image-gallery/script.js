bc481a44bc4cf9eaf910840ca5841a00cbef7346
Elen7371 <elena.and.kam@gmail.com>
2024-10-07 14:29:10 +0300

    feat: update index.js

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -4,44 +4,75 @@ let url =
   "https://api.unsplash.com/search/photos?query=icecream&per_page=12&orientation=landscape&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw";
 
 let photoList = document.querySelectorAll(".net-image");
+let searchInput = document.querySelector("input");
 
-async function getData() {
-  const res = await fetch(url);
-  const data = await res.json();
-  const results = data.results;
-  showData(results);
-}
+// async function getData() {
+//   const res = await fetch(url);
+//   const data = await res.json();
+// const results = data.results;
+// showData(results);
+//   showData(data);
 
-getData();
+//   console.log(data);
+// }
 
-function showData(results) {
-  for (let i = 0; i < results.length; i++) {
-    photoList[i].src = `${results[i].urls.regular}`;
-    photoList[i].alt = `${results[i].alt_description}`;
+// getData();
+
+// function showData(results) {
+//   for (let i = 0; i < results.length; i++) {
+//     photoList[i].src = `${results[i].urls.regular}`;
+//     photoList[i].alt = `${results[i].alt_description}`;
+//   }
+// }
+
+function showData(data) {
+  for (let i = 0; i < data.results.length; i++) {
+    photoList[i].src = `${data.results[i].urls.regular}`;
+    photoList[i].alt = `${data.results[i].alt_description}`;
   }
 }
 
-// грустная история о том, как я пыталась, но не смогла(((
-
-// let userQuery = ''
-
-// let input = document.querySelector("input");
-// console.log(input.value)
-
-//   input.onchange = function () {
-//     url = `https://api.unsplash.com/search/photos?${input.value}&per_page=12&orientation=landscape&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw`;
-// };
-
-// let form = document.querySelector("form");
-
-// form.addEventListener("submit", (event) => {
-//   let submitter = event.submitter;
-//   let handler = submitter.id;
-
+// function getUrls() {
+//   if (searchValue.value === "") {
+//     return url;
+//   } else {
+//     return (url = `https://api.unsplash.com/search/photos?query=${searchValue.value}&per_page=12&orientation=landscape&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw"`);
+//   }
 // }
-// )
 
-// const data = await getData()
-// showData(data)
+// searchInput.addEventListener("input", function (event) {
+//   if (event.target.value !== "") {
+//     return url = `https://api.unsplash.com/search/photos?query=${searchValue.value}&per_page=12&orientation=landscape&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw"`;
+//   }
+// });
 
-// await getData().then(showData);
+function getSearch(event) {
+  event.preventDefault();
+  if (searchInput.value !== "") {
+    return (url = `https://api.unsplash.com/search/photos?query=${searchValue.value}&per_page=12&orientation=landscape&client_id=33p-DkyYtVILyrQwP2JZUVxMlu8DUMtaRLZ-dtqtWYw"`);
+  } else {
+    return url;
+  }
+}
+
+async function getData(e) {
+  const res = await fetch(getSearch(e));
+  const data = await res.json();
+  // const results = data.results;
+  // showData(results);
+  showData(data);
+
+  console.log(data);
+}
+
+// getData();
+
+// function alarmTest(event) {
+//   alert("сщ мною вот что приключилось" + event.type);
+// }
+
+// searchInput.addEventListener("input", alarmTest);
+
+// searchInput.addEventListener("submit", getData);
+document.addEventListener("DOMContentLoaded", getData);
+
